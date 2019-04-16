32ae25ec4cfe8b955196083f320626b73ce42550
Jeff Diederiks <jdiederiks@psi-it.com>
2019-04-16 00:33:30 -0400

    Edited terraform package

--- terraform/main.go
+++ terraform/main.go
@@ -1,6 +1,9 @@
 package terraform
 
 import (
+  "fmt"
+  "time"
+
   "github.com/PyramidSystemsInc/go/commands"
   "github.com/PyramidSystemsInc/go/errors"
   "github.com/PyramidSystemsInc/go/str"
@@ -16,7 +19,7 @@ func VerifyInstallation() {
 }
 
 
-// Initializes the terraform directory checks for *.tf files and processes them
+// Initializes the terraform directory, checks for *.tf files, and processes them
 func Initialize(directoryToRunFrom string) string {
   output, err := commands.Run("terraform init -input=false", directoryToRunFrom)
   if err != nil {
@@ -25,3 +28,31 @@ func Initialize(directoryToRunFrom string) string {
   return output
 }
 
+// Creates a tfplan file with a detailed specification of what Terraform would create given the set of *.tf files
+func Plan(directoryToRunFrom string, cfg map[string]string) string {
+  var variables string
+  for key, value := range cfg {
+    variables = str.Concat(variables, "-var ", key, "=", value, " ")
+  }
+  planCommand := str.Concat("terraform plan ", variables, "-out tfplan")
+  output, err := commands.Run(planCommand, directoryToRunFrom)
+  if err != nil {
+    errors.LogAndQuit(str.Concat("ERROR: Initializing Terraform failed with the following error: ", err.Error()))
+  }
+  return output
+}
+
+// Creates resources detailed in the tfplan file (created using the `terraform plan` command
+func Apply(directoryToRunFrom string) string {
+  defer timeTrack(time.Now(), "Terraform apply")
+  output, err := commands.Run("terraform apply -input=false tfplan", directoryToRunFrom)
+  if err != nil {
+    errors.LogAndQuit(str.Concat("ERROR: Applying the Terraform plan failed with the following error: ", err.Error()))
+  }
+  return output
+}
+
+func timeTrack(start time.Time, name string) {
+  elapsed := time.Since(start)
+  fmt.Sprintf("%s took %s", name, elapsed)
+}
