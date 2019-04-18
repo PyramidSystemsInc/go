b4721fa5015e08225cbbe47d0e21f709e724d881
Michael McNairy <mmcnairy@psi-it.com>
2019-04-18 08:48:57 -0400

    fixed alias syntax

--- aws/kms/main.go
+++ aws/kms/main.go
@@ -30,7 +30,7 @@ func CreateEncryptionKey(awsSession *session.Session, k string, v string) (key s
 		os.Exit(1)
 	}
 
-	alias := "/pac/" + v
+	alias := "alias/pac/" + v
 
 	_, err = kmsClient.CreateAlias(&kms.CreateAliasInput{
 		AliasName:   aws.String(alias),
