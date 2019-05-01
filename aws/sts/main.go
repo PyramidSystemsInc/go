1d8509866f63e5244cf675c91248f2ef8e585dbe
Michael McNairy <mmcnairy@psi-it.com>
2019-05-01 12:26:50 -0400

    added GetAccountID function

---
+++ aws/sts/main.go
@@ -0,0 +1,35 @@
+package sts
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sts"
+)
+
+// GetAccountID returns AWS account ID of the account being used to call it.
+func GetAccountID() string {
+	svc := sts.New(session.New())
+	input := &sts.GetCallerIdentityInput{}
+
+	result, err := svc.GetCallerIdentity(input)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			fmt.Println(err.Error())
+		}
+		return "unable to get caller identity"
+	}
+
+	r, _ := json.Marshal(result.Account)
+
+	return string(r)
+}
