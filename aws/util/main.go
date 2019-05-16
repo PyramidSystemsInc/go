610dc1ad3d63115bb5aec1744c641120dfbac187
Michael McNairy <mmcnairy@psi-it.com>
2019-05-16 15:43:15 -0400

    added GetPublicIP function

--- aws/util/main.go
+++ aws/util/main.go
@@ -1,10 +1,31 @@
 package util
 
 import (
-  "strings"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
 )
 
+// IsArn checks if a string starts with `arn:aws:`.
 func IsArn(possibleArn string) bool {
-  arnPrefix := "arn:aws:"
-  return strings.HasPrefix(possibleArn, arnPrefix)
+	arnPrefix := "arn:aws:"
+	return strings.HasPrefix(possibleArn, arnPrefix)
+}
+
+// GetPublicIP sends a request to https://www.ipify.org/ for the end user's local ip address in text format.
+func GetPublicIP() string {
+	resp, err := http.Get("https://api.ipify.org")
+	if err != nil {
+		log.Fatalf("unable to get IP address: %v\n", err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("unable to read response: %v\n", err)
+	}
+
+	return string(body)
 }
