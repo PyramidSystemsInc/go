6f46388fff0e26e24441e6fda2cf89012cca8e7d
Rachel Wright <rwright@psi-it.com>
2019-06-06 09:02:42 -0400

    updated logger to accept multiple strings

--- logger/main.go
+++ logger/main.go
@@ -1,53 +1,54 @@
 package logger
 
 import (
-  "fmt"
-  "os"
-  "time"
+	"fmt"
+	"os"
+	"strings"
+	"time"
 )
 
 type LogLevel struct {
-  Info  bool
-  Warn  bool
-  Err   bool
+	Info bool
+	Warn bool
+	Err  bool
 }
 
 var logLevel LogLevel
 
 // Err - Logs and error from a string
-func Err(message string) {
-  if (logLevel.Err) {
-    fmt.Fprintln(os.Stderr, "[ " + timestamp() + "   ERROR ]: " + message)
-  }
+func Err(message ...string) {
+	if logLevel.Err {
+		fmt.Fprintln(os.Stderr, "[ "+timestamp()+"   ERROR ]: "+strings.Join(message, " "))
+	}
 }
 
 // Info - Logs output as information
-func Info(message string) {
-  if (logLevel.Info) {
-    fmt.Println("[ " + timestamp() + "    INFO ]: " + message)
-  }
+func Info(message ...string) {
+	if logLevel.Info {
+		fmt.Println("[ " + timestamp() + "    INFO ]: " + strings.Join(message, " "))
+	}
 }
 
 // SetLogLevel - Sets the maximum level of logging desired. All types more frequent than the type specified will not be output
 func SetLogLevel(level string) {
-  if (level == "info" || level == "warn" || level == "err") {
-    logLevel = LogLevel{
-      Info:  level == "info",
-      Warn:  level == "info" || level == "warn",
-      Err:   true,
-    }
-  } else {
-    fmt.Println("The valid log types are: 'info', 'warn', and 'err'")
-  }
+	if level == "info" || level == "warn" || level == "err" {
+		logLevel = LogLevel{
+			Info: level == "info",
+			Warn: level == "info" || level == "warn",
+			Err:  true,
+		}
+	} else {
+		fmt.Println("The valid log types are: 'info', 'warn', and 'err'")
+	}
 }
 
 // Warn - Logs output as a warning
-func Warn(message string) {
-  if (logLevel.Warn) {
-    fmt.Println("[ " + timestamp() + " WARNING ]: " + message)
-  }
+func Warn(message ...string) {
+	if logLevel.Warn {
+		fmt.Println("[ " + timestamp() + " WARNING ]: " + strings.Join(message, " "))
+	}
 }
 
 func timestamp() string {
-  return time.Now().UTC().Format(time.RFC3339)
+	return time.Now().UTC().Format(time.RFC3339)
 }
