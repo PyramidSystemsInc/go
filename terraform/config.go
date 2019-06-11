e697a2d719d8e899cdc736cb31e6bc05fdabdef8
Rachel Wright <rwright@psi-it.com>
2019-06-11 13:48:38 -0400

    Moved config to main project

--- terraform/config.go
+++
@@ -1,7 +0,0 @@
-package terraform
-
-// AWSVersion - Version of the Terraform AWS plugin
-var AWSVersion string = "1.60"
-
-// TemplateVersion - Version of the Terraform templating
-var TemplateVersion string = "2.1"
